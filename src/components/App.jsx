8141622615bda3bd9ef2baf6a406c10db8208cb8
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-07-11 21:45:48 +0300

    Добавил кнопку, поиграл с состоянем

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../stylesheets/body.css';
 
-const App = () => {
-  return (
-    <div className="body">
-      <p className="body__p">EASY PEASY LEMON SQUEEZY!</p>
-      <button className="body__button">PRESS THIS BUTTON</button>
-    </div>
-  )
+class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        pOpened: true
+      }
+      this.pOpen = this.pOpen.bind(this);
+  }
+
+  pOpen(){
+    this.setState(
+      {
+        pOpened: !this.state.pOpened
+      }
+    )
+    console.log(this.state)
+  }
+
+  render() {
+      const {pOpen} = this;
+      const {pOpened} = this.state;
+      return (
+        <div className="body">
+          <p className={pOpened ? "body__p" : "body__p hidden"}>EASY PEASY LEMON SQUEEZY!</p>
+          <button onClick={pOpen} className="body__button">PRESS THIS BUTTON</button>
+        </div>
+      )
+  }
 };
 
 export default App;
