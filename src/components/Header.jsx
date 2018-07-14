84c0d0257bc93902f4d89ea67ccf5195de455da0
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-07-14 20:50:28 +0300

    Уменьшил размер кнопок

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ const Header = () => {
             </div>
             <nav>
               <ul className="header__nav__menu">
-                <li><Button variant="flat" className="button">about me</Button></li>
-                <li><Button variant="flat">email</Button></li>
-                <li><Button variant="flat"><a href="https://vk.com/id239609752" target="_blank">vk.com</a></Button></li>
+                <li><Button size = "small" variant="flat" className="button">about me</Button></li>
+                <li><Button size="small" variant="flat">email</Button></li>
+                <li><Button size="small" variant="flat"><a href="https://vk.com/id239609752" target="_blank">vk.com</a></Button></li>
               </ul>
             </nav>
             
