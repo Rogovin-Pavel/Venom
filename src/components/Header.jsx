cfd8081e93e3ef5427cd923cea5fe8a563b3623c
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-07-28 21:59:09 +0300

    Added phone button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ const Header = () =>
             <ul className="header__nav__menu">
                 <li>ABOUT ME</li>
                 <li>EMAIL</li>
+                <li>PHONE</li>
                 <li><a href="https://vk.com/id239609752" target="_blank">VK.COM</a></li>
             </ul>
         </nav> 
